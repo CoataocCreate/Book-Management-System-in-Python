39c201eb8acc069bc69808de728ff8578fc2765e
Coataoc <160588631+CoataocCreate@users.noreply.github.com>
2024-06-14 16:22:21 -0700

    Create book.py

---
+++ book.py
@@ -0,0 +1,11 @@
+# book.py
+
+class Book:
+    def __init__(self, title, author, genre, year):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.year = year
+
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.year})"
